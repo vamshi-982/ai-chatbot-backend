3b69825a4fb0fc9bfb1dd249dc6f0c2377ba1e3c
Siddapuram Vamshi <vamshisiddapuram982@gmail.com>
2026-03-03 10:16:31 +0530

    Added enablewebsecurity package

--- src/main/java/com/grovyn/chatbot/security/SecurityConfig.java
+++ src/main/java/com/grovyn/chatbot/security/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.
 UsernamePasswordAuthenticationFilter;
-
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 @Configuration
 public class SecurityConfig {
 
