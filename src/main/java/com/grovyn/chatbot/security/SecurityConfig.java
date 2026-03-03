45d9ef6d4b9e8655e4eb542321c90fa3c2fb8987
Siddapuram Vamshi <vamshisiddapuram982@gmail.com>
2026-03-03 10:25:35 +0530

    Added @EnableWebSecurity in spring config

--- src/main/java/com/grovyn/chatbot/security/SecurityConfig.java
+++ src/main/java/com/grovyn/chatbot/security/SecurityConfig.java
@@ -7,7 +7,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.
 UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
